3a9cac69a96f693609a22b3dc2652e904729caa1
Sergio Jair Cerezo Vallejo <sergio.jair.cerezo@gmail.com>
2018-11-13 14:09:08 -0600

    Installing react-native-elements in RN project

--- nwrn/screens/HelloWorldScreen.js
+++ nwrn/screens/HelloWorldScreen.js
@@ -8,6 +8,7 @@
 
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, Button } from 'react-native';
+import { Icon } from 'react-native-elements';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -23,6 +24,7 @@ export default class HelloWorldScreen extends Component {
 
   static navigationOptions = {
     title: 'First Screen',
+    headerLeft: <Icon name="menu" size={35} onPress={ () => navigation.navigate('DrawerOpen') } />
   };
 
   render() {
