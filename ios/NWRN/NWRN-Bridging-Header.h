56cd7f1381eb5fa581881e7ced1d24edb62f388d
Sergio Jair Cerezo Vallejo <sergio.jair.cerezo@gmail.com>
2018-11-13 14:45:58 -0600

    Installing react-native-elements in iOS project

---
+++ ios/NWRN/NWRN-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  NWRN-Bridging-Header.h
+//  NWRN
+//
+//  Created by Sergio Jair Cerezo Vallejo on 11/13/18.
+//  Copyright © 2018 Sergio Jair Cerezo Vallejo. All rights reserved.
+//
+
+#ifndef NWRN_Bridging_Header_h
+#define NWRN_Bridging_Header_h
+
+#import "React/RCTBridgeModule.h"
+#import "React/RCTBundleURLProvider.h"
+#import "React/RCTRootView.h"
+
+#endif /* NWRN_Bridging_Header_h */
