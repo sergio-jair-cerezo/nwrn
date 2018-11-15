3ab0a60cd1d8c3c135aed54dc00511f2374f4abc
Sergio Jair Cerezo Vallejo <scerezo@tiempodevelopment.com>
2018-11-15 12:41:18 -0600

    Refactor MainActivity -> KotlinFirstActivity

--- android/app/src/main/java/com/sf/nwrn/Activities/KotlinFirstActivity.kt
+++ android/app/src/main/java/com/sf/nwrn/Activities/KotlinFirstActivity.kt
@@ -1,23 +1,22 @@
 package com.sf.nwrn.Activities
 
+import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
-import android.support.v7.app.AppCompatActivity
-import android.view.Menu
 import android.view.MenuItem
 import com.sf.nwrn.R
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_kotlin_first.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.content_kotlin_first.*
 
-class MainActivity : BaseActivity() {
+class KotlinFirstActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_kotlin_first)
         setSupportActionBar(toolbar)
 
         val toggle = ActionBarDrawerToggle(this, getDrawerLayout(), toolbar,
@@ -28,6 +27,11 @@ class MainActivity : BaseActivity() {
         getNavigationView()?.setNavigationItemSelectedListener(this)
 
         title = getString(R.string.activity_first)
+
+        btn_go_to_second_act.setOnClickListener {
+            val intent = Intent(this, KotlinSecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onBackPressed() {
